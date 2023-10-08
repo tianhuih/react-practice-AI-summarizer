b1e5ee24275d3dbe4c2c821b5aea6cedfe7f9e9f
Tianhui Huang <t.tianhui.huang@gmail.com>
2023-10-08 13:45:42 -0700

    implemented URL form

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,36 @@
-import React from "react";
-import { logo } from "../assets/";
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
   return (
-
-  )
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+          <button type="submit" className="submit_btn">
+            ↵
+          </button>
+        </form>
+        {/* Browse URL History */}
+      </div>
+      {/* Browse URL History */}
+    </section>
+  );
 };
 
 export default Demo;
