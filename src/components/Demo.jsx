5b264810a47d6edabf5e76619c2ccea246f3442b
Tianhui Huang <t.tianhui.huang@gmail.com>
2023-10-08 14:29:47 -0700

    implement api call on submit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,42 +1,50 @@
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../Services/article";
 
 const Demo = () => {
-    const [article, setArticle] = useState({ url: "", summary: "" });
+  const [article, setArticle] = useState({ url: "", summary: "" });
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-    const handleSubmit = async (e) => {
-      alert("hello");
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
+  };
 
-    return (
-      <section className="mt-16 w-full max-w-xl">
-        <div className="flex flex-col w-full gap-2">
-          <form
-            className="relative flex justify-center items-center"
-            onSubmit={handleSubmit}
-          >
-            <img
-              src={linkIcon}
-              alt="link_icon"
-              className="absolute left-0 my-2 ml-3 w-5"
-            />
-            <input
-              type="url"
-              placeholder="Enter a URL"
-              value={article.url}
-              onChange={(e) => setArticle({ ...article, url: e.target.value })}
-              required
-              className="url_input peer"
-            />
-            <button type="submit" className="submit_btn">
-              ↵
-            </button>
-          </form>
-          {/* Browse URL History */}
-        </div>
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+          <button type="submit" className="submit_btn">
+            ↵
+          </button>
+        </form>
         {/* Browse URL History */}
-      </section>
-    );
+      </div>
+      {/* Browse URL History */}
+    </section>
+  );
 };
 
 export default Demo;
--- src/services/article.js
+++ src/services/article.js
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com",
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
     prepareHeaders: (headers) => {
       headers.set("X-RapidAPI-Key", rapidApiKey);
       headers.set(
@@ -17,7 +17,7 @@ export const articleApi = createApi({
   endpoints: (builder) => ({
     getSummary: builder.query({
       query: (params) =>
-        `/summarizer?${encodeURIComponent(params.articleUrl)}&length=3`,
+        `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
   }),
 });
