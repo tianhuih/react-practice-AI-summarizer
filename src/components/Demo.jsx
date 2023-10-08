3ab9938a042403138926ed5c2fc4e5ebd5f0444c
Tianhui Huang <t.tianhui.huang@gmail.com>
2023-10-08 13:49:37 -0700

    added handle submit function

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,35 +2,41 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return (
-    <section className="mt-16 w-full max-w-xl">
-      <div className="flex flex-col w-full gap-2">
-        <form
-          className="relative flex justify-center items-center"
-          onSubmit={() => {}}
-        >
-          <img
-            src={linkIcon}
-            alt="link_icon"
-            className="absolute left-0 my-2 ml-3 w-5"
-          />
-          <input
-            type="url"
-            placeholder="Enter a URL"
-            value=""
-            onChange={() => {}}
-            required
-            className="url_input peer"
-          />
-          <button type="submit" className="submit_btn">
-            ↵
-          </button>
-        </form>
+    const [article, setArticle] = useState({ url: "", summary: "" });
+
+    const handleSubmit = async (e) => {
+      alert("hello");
+    };
+
+    return (
+      <section className="mt-16 w-full max-w-xl">
+        <div className="flex flex-col w-full gap-2">
+          <form
+            className="relative flex justify-center items-center"
+            onSubmit={handleSubmit}
+          >
+            <img
+              src={linkIcon}
+              alt="link_icon"
+              className="absolute left-0 my-2 ml-3 w-5"
+            />
+            <input
+              type="url"
+              placeholder="Enter a URL"
+              value={article.url}
+              onChange={(e) => setArticle({ ...article, url: e.target.value })}
+              required
+              className="url_input peer"
+            />
+            <button type="submit" className="submit_btn">
+              ↵
+            </button>
+          </form>
+          {/* Browse URL History */}
+        </div>
         {/* Browse URL History */}
-      </div>
-      {/* Browse URL History */}
-    </section>
-  );
+      </section>
+    );
 };
 
 export default Demo;
